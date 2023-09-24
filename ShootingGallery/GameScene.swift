69ab231a23534e53fb53ed9408aac33731f646da
Yuga Samuel <yugasamuel2002@gmail.com>
2023-09-24 13:36:08 +0700

    Spawn ducks with Timer and add velocity so it can move

--- ShootingGallery/GameScene.swift
+++ ShootingGallery/GameScene.swift
@@ -8,14 +8,54 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    var gameTimer: Timer?
+    
+    let possibleDucks = ["duck_outline_brown", "duck_outline_white", "duck_outline_yellow"]
     
     override func didMove(to view: SKView) {
-        backgroundColor = .white
+        gameTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(createDuck), userInfo: nil, repeats: true)
         
+        physicsWorld.gravity = CGVector(dx: 0, dy: 0)
+        physicsWorld.contactDelegate = self
+    }
+    
+    @objc func createDuck() {
+        let position: CGPoint!
+        let zPosition: CGFloat!
+        let velocity: CGVector!
+        let xScale: CGFloat!
         
+        let row = Int.random(in: 1...3)
+        if row == 1 {
+            position = CGPoint(x: -75, y: 200)
+            velocity = CGVector(dx: 300, dy: 0)
+            zPosition = -4
+            xScale = 1
+        } else if row == 2 {
+            position = CGPoint(x: 1099, y: 325)
+            velocity = CGVector(dx: -300, dy: 0)
+            zPosition = -6
+            xScale = -1
+        } else {
+            position = CGPoint(x: -75, y: 450)
+            velocity = CGVector(dx: 300, dy: 0)
+            zPosition = -8
+            xScale = 1
+        }
         
+        guard let duck = possibleDucks.randomElement() else { return }
         
+        let sprite = SKSpriteNode(imageNamed: duck)
+        sprite.physicsBody = SKPhysicsBody(texture: sprite.texture!, size: sprite.size)
+        sprite.position = position
+        sprite.zPosition = zPosition
+        sprite.xScale = xScale
+        sprite.physicsBody?.velocity = velocity
+
+        addChild(sprite)
         
     }
 }
+
+
